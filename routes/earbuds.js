014c42ebde6a5a666ef01e986391c55870aa115e
SairakeshGajavalli <gajavallisairakesh1999@gmail.com>
2023-11-12 16:58:31 -0600

    update

--- routes/earbuds.js
+++ routes/earbuds.js
@@ -3,6 +3,6 @@ const earbud_controller= require('../controllers/earbud')
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', earbud_controller.earbud_view_all_Page);
+router.get('/',  earbud_controller.earbud_view_all_Page);
 
 module.exports = router;
