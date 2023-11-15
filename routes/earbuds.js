067bb3b4b436ec9b5bf8478329a6fa5e05f31b44
SairakeshGajavalli <gajavallisairakesh1999@gmail.com>
2023-11-15 16:02:20 -0600

    update

--- routes/earbuds.js
+++ routes/earbuds.js
@@ -6,5 +6,8 @@ var router = express.Router();
 router.get('/',  earbud_controller.earbud_view_all_Page);
 /* GET detail costume page */
 router.get('/detail', earbud_controller.earbud_view_one_Page);
+/* GET create costume page */
+router.get('/create', earbud_controller.earbud_create_Page);
+
 
 module.exports = router;
